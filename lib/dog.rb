5f269a964101a325296b7dd4825ac45722c4a296
Sydney Garay <sgg2123@columbia.edu>
2018-04-25 21:19:46 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,25 @@
-class Dog 
+class Person
+ 
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+  end
+ 
+  def name
+    @name
+  end
+ 
+  def name=(new_name)
+    @name = new_name
+  end
   
+  def breed
+    @breed
+  end
+ 
+  def breed=(new_breed)
+    @name = new_breed
+  end
+ 
+end
+
